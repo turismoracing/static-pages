effccff238031c3bcd9c6d4afec4f564a9886a4b
turismo <email@turismoracing.com>
2022-12-20 16:06:51 -0500

    Responsive changes to the delivery page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,24 @@ module.exports = {
     fontFamily: {
       display: ["Montserrat", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        side: {
+          "0%": {
+            transform: "translateX(-100%)",
+            opacity: 0,
+           },
+
+           "100%": {
+            transform: "translateX(0)",
+            opacity: 1,
+           },
+        },
+      },
+      animation: {
+          "from-the-side": "side 1.7s ease 0",
+      },
+    },
   },
   plugins: [],
 }
